061d67c6b7eb42910cdd4e814358fdd6971cd9e0
joel92MM <joel_tf_92@hotmail.com>
2021-10-12 20:17:31 +0100

    Ejercicio1

--- DSW/Tema03/Ejercicios.php
+++ DSW/Tema03/Ejercicios.php
@@ -14,6 +14,65 @@
         </h1>
         <br/>
         <hr/>
-        
+            <?php
+                if($_POST){
+                    require_once("../Tema03/FuncionesAuxiliares/funcionesAuxiliares");
+                    $a=$_POST['a'];$b=$_POST['b'];
+                    $c=sumaMatrices($a, $b);
+
+                    echo "<table>
+                            <tr>
+                                <td>"
+                                imprimirMatriz($a);
+                    echo        "</td>
+                                <td>+</td>
+                                <td>";
+                                imprimirMatriz($b);
+                    echo        
+                                "</td>
+                                <td>=</td>
+                                <td>"
+                                imprimirMatriz($c);
+                    echo            "</td>
+                            </tr>
+                        </table>"
+                }else if($_GET){
+                    require_once("../Tema03/FuncionesAuxiliares/funcionesAuxiliares")
+                    $filas=$_POST['filas'];$cols=$_POST['cols'];
+                    echo "<form action='' method='POST' enctype='multipart/form-data'";
+
+                    echo "<table>
+                            <tr>
+                                <td>"
+                                imprimirMatriz("a",$filas,$cols);
+                    echo        "</td>
+                                <td>+</td>
+                                <td>";
+                                imprimirMatriz("b",$filas,$cols);
+                    echo    "   </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <button type='submit'>Enviar</button>
+                                </td>
+                                <td>
+                                    <button type='reset'>Borrar Formulario</button>
+                                </td>
+                            </tr>
+                        </table>
+                        </form>";
+                }else{
+                    ?>
+                    <form action="" method="GET" enctype="multipart/form-data">
+                        <table>
+                            <tr><td><label for="filas">Filas:</label></td><td><input name="filas" id="filas" type="number" min="1" max="20" size="4"></td></tr>
+                            <tr><td><label for="cols">Cols:</label></td><td><input name="cols" id="cols" type="number" min="1" max="20" size="4"></td></tr>                                  
+                            <tr><td><button type="submit">Enviar</button></td><td><button type="reset">Borrar Formulario</td></tr>                                  
+                        </table>    
+                    </form>
+
+                    <?php
+                }
+            ?>
     </body>
 </html>
--- DSW/Tema03/FuncionesAuxiliares/funcionesAuxiliares.php
+++ DSW/Tema03/FuncionesAuxiliares/funcionesAuxiliares.php
@@ -13,7 +13,29 @@
         return $r;
     }
     //$a es una matriz bidimensional
-    funct
+    function imprimirMatriz(¢a){
+        echo "<table border='1'>\n";
+        for ($i=0; $i <count($a) ; $i++) { 
+            echo"   <tr>\n";
+            for ($j=0; $j < count($a[0]); $j++) { 
+                echo "<td style='padding: 3px; text align: right;'>",$a[$i][$j]"</td>"
+            }
+                echo "</tr>"
+        }
+        echo "</table>";
+    }
 
     //funcion que imprime el numero de filas y de columnas
+    function imprimeTablazMatriz($nombre,$filas,$cols){
+        echo "<table border='1'>\n";
+        for ($i=0; $i < $filas ; $i++) { 
+            echo"   \n<tr>";
+            for ($j=0; $j < $cols; $j++) { 
+                $valor=$i*$cols+$j;
+                echo "<td> <input type='number' name='$nombre"." [$i][]' value='$valor' maxlenght='4' size='4'/></td>"
+            }
+                echo "</tr>"
+        }
+        echo "</table>";
+    }
 ?>
