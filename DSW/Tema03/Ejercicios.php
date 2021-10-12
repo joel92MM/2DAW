d359c6d48a25c9c1de3bcc61dd1ecdc143d553c0
joel92MM <joel_tf_92@hotmail.com>
2021-10-12 16:48:17 +0100

    Edicion

---
+++ DSW/Tema03/Ejercicios.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+    <html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>
+        Programa que reciba por "GET" número de filas (f) y número de columnas (c) y
+        muestre formulario para pedir 2 matrices de fxc y que al enviar los datos muestre la suma de ambas
+        (ampliar para el producto de matrices)
+        </h1>
+        <br/>
+        <hr/>
+        
+    </body>
+</html>
