66ea9e962cb14232ee44740f0ff1d62aebd715d5
joel92MM <joel_tf_92@hotmail.com>
2021-11-05 01:28:45 +0000

    Ejemplo

--- DEW/AUT02_04/bolas.js
+++ DEW/AUT02_04/bolas.js
@@ -1,3 +1,26 @@
 function bola(){
+    //creamos el objeto bola
+    class bola{
+        
+
+        constructor (coordX,coordY,velX,velY,color,size){
+            this.coordX=coordX;
+            this.coordY=coordY;
+            this.velX=velX;
+            this.color=color;
+            this.size=size;
+        }
+
+        dibuja(){
+            console.log("Coordenada X: " + this.coordX+ ", Coordenada Y:"+ this.coordY);
+        }
+        mover(){
+            this.coordX+=this.velX;
+            this.coordY+=this.velY;
+        }
+
+        }
+    
+    
     
 }
